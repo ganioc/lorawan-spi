62de9c9307b7f9bca79a0d63bec8ec2fcbb0e0a1
yango3 <o3.yang@hotmail.com>
2022-03-11 22:54:48 +0800

    add uart rx

--- libspi/README.md
+++ libspi/README.md
@@ -2,4 +2,5 @@
 - 测试树莓派下的串口通讯
 
 ./libspi -d /dev/ttyUSB0
+$ ./libspi -d /dev/ttyUSB0 --tx
 
--- libspi/src/libspi.c
+++ libspi/src/libspi.c
@@ -16,6 +16,8 @@ Description: USB SPI lib
 #include <getopt.h>     /* getopt_long */
 #include <fcntl.h>
 #include <termios.h>
+#include <unistd.h>
+#include <errno.h>
 
 #include "loragw_hal.h"
 #include "loragw_reg.h"
@@ -46,9 +48,77 @@ int open_uart(char* path){
 
     return (fd);
 }
+int config_uart(int fd){
+    struct termios tty;
+    if(tcgetattr(fd, &tty) != 0) {
+        printf("Error %i from tcgetattr: %s\n", errno, strerror(errno));
+    }
+    tty.c_cflag &= ~PARENB; // Clear parity bit, disabling parity (most common)
+    tty.c_cflag &= ~CSTOPB; // Clear stop field, only one stop bit used in communication (most common)
+    tty.c_cflag &= ~CSIZE; // Clear all the size bits, then use one of the statements below
+    tty.c_cflag |= CS8; // 8 bits per byte (most common)
+    tty.c_cflag &= ~CRTSCTS; // Disable RTS/CTS hardware flow control (most common)
+    tty.c_cflag |= CREAD | CLOCAL; // Turn on READ & ignore ctrl lines (CLOCAL = 1)
+    tty.c_lflag &= ~ICANON; //Disabling Canonical Mode
+    tty.c_lflag &= ~ECHO; // Disable echo
+    tty.c_lflag &= ~ECHOE; // Disable erasure
+    tty.c_lflag &= ~ECHONL; // Disable new-line echo
+    tty.c_lflag &= ~ISIG; // Disable interpretation of INTR, QUIT and SUSP
+    tty.c_iflag &= ~(IXON | IXOFF | IXANY); // Turn off s/w flow ctrl
+    tty.c_iflag &= ~(IGNBRK|BRKINT|PARMRK|ISTRIP|INLCR|IGNCR|ICRNL); // Disable any special handling of received bytes
+    tty.c_oflag &= ~OPOST; // Prevent special interpretation of output bytes (e.g. newline chars)
+    tty.c_oflag &= ~ONLCR; // Prevent conversion of newline to carriage return/line feed
+
+    tty.c_cc[VTIME] = 10;    // Wait for up to 1s (10 deciseconds), returning as soon as any data is received.
+    tty.c_cc[VMIN] = 0;
+
+    // Set in/out baud rate to be 9600
+    // B0,  B50,  B75,  B110,  B134,  B150,  B200, B300, B600, B1200, B1800, B2400, B4800, B9600, B19200, B38400, B57600, B115200, B230400, B460800
+    cfsetispeed(&tty, B9600);
+    cfsetospeed(&tty, B9600);
+
+    if (tcsetattr(fd, TCSANOW, &tty) != 0) {
+        printf("Error %i from tcsetattr: %s\n", errno, strerror(errno));
+    }
+
+
+}
 int close_uart(int fd){
     return close(fd);
 
+}
+int tx_task(int fd){
+    unsigned char msg[] = {0x01, 0x02, 0x03};
+    int n;
+
+    for(int i=0; i< 10; i++){
+        n = write(fd, msg, sizeof(msg));
+        printf("Write out: %d\n", n);
+        if(n != sizeof(msg)){
+            printf("Error sending %n , lost %d\n", 
+                sizeof(msg), 
+                sizeof(msg)-n);
+        }
+        sleep(1);
+    }
+    
+    return 0;
+}
+int rx_task(int fd){
+    int n;
+    unsigned char buf[1025];
+    while(true){
+        n=read(fd,buf,1204);
+        if(n){
+            printf("RX: %d\n", n);
+        }else if(n<0){
+            printf("Error: receiving error\n");
+        }else{
+            printf("RX: timeout\n");
+        }
+    }
+
+    return 0;
 }
 int main(int argc, char **argv){
     unsigned int arg_u;
@@ -59,6 +129,8 @@ int main(int argc, char **argv){
     uint64_t eui;
     char DEVICE_NAME[128];
     int fd;
+    int tx_enabled = 0;
+    int rx_enabled = 0;
 
     int i;
     /* Parameter parsing */
@@ -66,6 +138,8 @@ int main(int argc, char **argv){
     static struct option long_options[] = {
         {"help",0,0,'h'},
         {"device",1,0,'d'},
+        {"tx",0,0,'t'},
+        {"rx",0,0,'r'},
         {0, 0, 0, 0}
     };
 
@@ -79,6 +153,10 @@ int main(int argc, char **argv){
             case 'd':
                     // printf("device is %s\n", optarg);
                     sprintf(DEVICE_NAME, "%s", optarg);
+                    break;
+            case 't': tx_enabled = 1;
+                    break;
+            case 'r': rx_enabled = 1;
                     break;
             default: // printf("Unknown\n");
                     return -1;
@@ -95,9 +173,24 @@ int main(int argc, char **argv){
         usage();
         return -1;
     }
+
+    if(tx_enabled && rx_enabled || (!tx_enabled && !rx_enabled)){
+        printf("--tx or --rx\n");
+        return -1;
+    }
+
     printf("Open uart: %s\n", DEVICE_NAME);
     fd = open_uart(DEVICE_NAME);
 
+    config_uart(fd);
+
+    if(tx_enabled){
+        tx_task(fd);
+    }else if(rx_enabled){
+        rx_task(fd);
+    }
+
+    
     printf("Close uart: %d\n", fd);
     close_uart(fd);
 
